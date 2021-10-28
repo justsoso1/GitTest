07d8b28907414401570988af80d783c6f7e8b783
buguoruci <buguoruci@atguigu.com>
2021-10-28 15:38:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName gitTest
+ * @Description TODO
+ * @Author 不过如此
+ * @Date 2021/10/28 15:00
+ * @Version 1.0
+ */
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("test 1");
+    }
+}

70ba2af39109f2f64a14ca25ad90bfca6a03e41a
buguoruci <buguoruci@atguigu.com>
2021-10-28 15:42:34 +0800

    fix-out commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -12,5 +12,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("test 1");
         System.out.println("test 2");
+        System.out.println("fix-out 1");
     }
 }

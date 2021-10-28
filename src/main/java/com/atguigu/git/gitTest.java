7369efff16f130d414a17bc49625c5fe8be7ba17
buguoruci <buguoruci@atguigu.com>
2021-10-28 15:40:26 +0800

    second commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -11,5 +11,6 @@ package com.atguigu.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("test 1");
+        System.out.println("test 2");
     }
 }
